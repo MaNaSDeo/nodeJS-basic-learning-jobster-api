ac92abe80e6ab10d837d51ccee0b52d6ec4d0269
MaNaSDeo <rmnslogin@gmail.com>
2024-04-04 20:06:52 +0530

    api rate limiter

--- app.js
+++ app.js
@@ -29,15 +29,8 @@ const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.static(path.resolve(__dirname, "./client/build")));
-/*
+
 app.set("trust proxy", 1);
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-  })
-);
-*/
 app.use(express.json());
 app.use(helmet());
 /*
