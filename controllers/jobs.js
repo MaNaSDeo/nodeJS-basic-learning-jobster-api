8acf7ff950f2e432e939268d5dd7bb51ed2c7eef
MaNaSDeo <rmnslogin@gmail.com>
2024-04-04 19:34:29 +0530

    pagination

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -31,9 +31,19 @@ const getAllJobs = async (req, res) => {
     result = result.sort("-position");
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result;
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
   // const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
-  res.status(StatusCodes.OK).json({ jobs });
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 const getJob = async (req, res) => {
   const {
